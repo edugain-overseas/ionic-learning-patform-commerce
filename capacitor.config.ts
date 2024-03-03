ad78848379cb16b6981558b699df93d19e878780
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2024-03-03 23:17:59 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-learning-platform-commercep',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
