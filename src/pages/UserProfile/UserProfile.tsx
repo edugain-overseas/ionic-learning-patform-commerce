ee34d353ece6f61080d79f8cd94b5a6d4e0f6696
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2024-04-15 15:46:22 +0300

    feature/certificateModal

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,17 +1,36 @@
-import { IonContent, IonIcon, IonImg, IonModal, IonPage } from "@ionic/react";
+import { IonContent, IonIcon, IonModal, IonPage } from "@ionic/react";
 import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { certificates } from "../../constants";
 import avatar from "../../assets/images/subject_image.png";
 import ClockIcon from "../../assets/icons/clock.svg";
+import DocumentDownloadIcon from "../../assets/icons/document-download.svg";
 import TaskProgressIcon from "../../assets/icons/task-progress.svg";
 import TaskCompletedIcon from "../../assets/icons/task-completed.svg";
 import Header from "../../components/Header/Header";
 import Avatar from "../../components/Avatar/Avatar";
 import TextOverrflowEllipsis from "../../components/TextOverrflowEllipsis/TextOverrflowEllipsis";
-import styles from "./UserProfile.module.scss";
 import Accordion from "../../components/Accordion/Accordion";
 import CircleProgressCard from "../../components/CircleProgressCard/CircleProgressCard";
-import { certificates } from "../../constants";
 import InfoBtn from "../../components/InfoBtn/InfoBtn";
+import CommonButton from "../../components/CommonButton/CommonButton";
+import styles from "./UserProfile.module.scss";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+import { Filesystem, Directory, Encoding } from "@capacitor/filesystem";
+
+const pagination = {
+  clickable: true,
+  renderBullet: function (_: any, className: string) {
+    return `<span class="${className} ${styles.customBullet}"></span>`;
+  },
+};
 
 const UserProfile: React.FC = () => {
   const [certificateIndex, setCertificateIndex] = useState(0);
@@ -22,7 +41,36 @@ const UserProfile: React.FC = () => {
     setIsOpenModalCerficate(true);
   };
 
-  console.log(isOpenModalCerficate);
+  const handleUploadCertificate = async () => {
+    const fileUrl = "https://pdfobject.com/pdf/sample.pdf";
+
+    try {
+      const response = await fetch(fileUrl);
+      const blob = await response.blob();
+
+      const fileName = "certificate.pdf";
+
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.setAttribute("download", fileName);
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+
+      // await Filesystem.writeFile({
+      //   path: fileName,
+      //   data: blob,
+      //   directory: Directory.Documents, // Save in downloads directory
+      //   encoding: Encoding.UTF8,
+      // });
+
+      // Show success message or perform any other action upon successful download
+      console.log("File downloaded successfully:", fileName);
+    } catch (error) {
+      console.error("Error downloading file:", error);
+    }
+  };
 
   return (
     <IonPage>
@@ -176,9 +224,42 @@ const UserProfile: React.FC = () => {
           breakpoints={[0, 1]}
           initialBreakpoint={1}
         >
-          {certificates[certificateIndex].images.map(({ src }, index) => (
-            <img src={src} key={index} />
-          ))}
+          <div className={styles.cetrificateModalWrapper}>
+            <div className={styles.certificatesModalHeader}>
+              <span>{certificates[certificateIndex].name}</span>
+            </div>
+            <Swiper
+              className={styles.swiper}
+              spaceBetween={24}
+              centeredSlides={true}
+              slidesPerView={"auto"}
+              pagination={pagination}
+              modules={[Pagination]}
+            >
+              {certificates[certificateIndex].images.map(({ src }, index) => (
+                <SwiperSlide key={index} className={styles.slide}>
+                  <img src={src} key={index} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <div className={styles.certificateModalDownload}>
+              <CommonButton
+                width={132}
+                height={32}
+                backgroundColor="transparent"
+                borderRadius={5}
+                border="1px solid #7E8CA8"
+                label="Certificate"
+                icon={
+                  <IonIcon
+                    src={DocumentDownloadIcon}
+                    className={styles.docDownloadIcon}
+                  />
+                }
+                onClick={handleUploadCertificate}
+              />
+            </div>
+          </div>
         </IonModal>
       </IonContent>
     </IonPage>
