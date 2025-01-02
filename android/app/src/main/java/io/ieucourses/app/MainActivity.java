4bd0aefa021f6639607990632dd74ebdcd0170eb
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2025-01-02 15:29:10 +0200

    fix: androidPlatform native elements

--- android/app/src/main/java/io/ieucourses/app/MainActivity.java
+++ android/app/src/main/java/io/ieucourses/app/MainActivity.java
@@ -1,12 +1,23 @@
 package io.ieucourses.app;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
+        }
+
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
             registerPlugin(com.getcapacitor.community.stripe.StripePlugin.class);
         }
 }
