750505470d14642f4bc2e489699c0e055bfa6d5b
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2025-08-28 14:31:58 +0300

    fix/vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
-import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy(), basicSsl()],
-  // test: {
-  //   globals: true,
-  //   environment: "jsdom",
-  //   setupFiles: "./src/setupTests.ts",
-  // },
+  plugins: [react(), legacy()],
   server: {
     allowedHosts: ['7dac-176-38-25-248.ngrok-free.app']
   },
