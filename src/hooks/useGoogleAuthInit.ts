f1fb91c1b778eaa5f40e7318b77dd43adde27ce8
Roman Polishchuk <polrmn@MacBook-Air-MacBook.local>
2026-05-06 14:51:58 +0300

    fix/goggleAuth ios

--- src/hooks/useGoogleAuthInit.ts
+++ src/hooks/useGoogleAuthInit.ts
@@ -1,20 +1,20 @@
-// import { Capacitor } from "@capacitor/core";
+import { Capacitor } from "@capacitor/core";
 import { GoogleAuth } from "@codetrix-studio/capacitor-google-auth";
 import { useEffect } from "react";
 
 export const useGoogleAuthInit = () => {
   useEffect(() => {
-    // GoogleAuth.initialize({
-    //   ...(Capacitor.isNativePlatform()
-    //     ? {}
-    //     : { clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID }),
-    //   scopes: ["email", "profile"],
-    //   grantOfflineAccess: false,
-    // });
     GoogleAuth.initialize({
-      clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+      ...(Capacitor.getPlatform() === "ios"
+        ? {}
+        : { clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID }),
       scopes: ["email", "profile"],
       grantOfflineAccess: false,
     });
+    // GoogleAuth.initialize({
+    //   clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    //   scopes: ["email", "profile"],
+    //   grantOfflineAccess: false,
+    // });
   }, []);
 };
